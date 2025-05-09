a41749608a56ffda305ad6cf0b7fd6f15c0346c0
Minihodari12 <152394434+Minihodari12@users.noreply.github.com>
2025-05-09 16:52:53 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'Arttur', // Your name
   ownerNumber: '+358453274235', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
